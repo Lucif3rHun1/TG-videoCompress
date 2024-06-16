07cb28c18584e3156ab6461ab815e5cf3740b518
Lucif3rHun1 <github1@lucif3rhun1.com>
2024-06-17 02:43:16 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -14,6 +14,12 @@
 # https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
 
 from decouple import config
+import logging
+
+# Configure logging
+LOGS = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 
 try:
     APP_ID = config("APP_ID", cast=int)
@@ -24,4 +30,7 @@ try:
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMB = config("THUMBNAIL")
 except Exception as e:
+    LOGS.info("Environment vars Missing")
+    LOGS.info("something went wrong")
+    LOGS.info(str(e))
     exit(1)
